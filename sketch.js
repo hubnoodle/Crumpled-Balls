7b3972bc8b6f4be4a6061f1a46627509724f850f
hubnoodle <76450902+hubnoodle@users.noreply.github.com>
2021-04-15 08:51:47 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,37 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var ground, paperBall, dustBinObject;
+
+function setup() {
+	createCanvas(1200, 400);
+
+	engine = Engine.create();
+	world = engine.world;
+
+	groundobj = new Ground(width/2,370,width,20);
+	paperBallobj = new Paper(200,150,70);
+	dustBinObject = new Dustbin(1000,360);
+	
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  
+  background(240);
+  
+  Engine.update(engine);
+  groundobj.display();
+  paperBallobj.display();
+  dustBinObject.display();
+}
+
+function keyPressed() {
+	if (keyCode === UP_ARROW) {
+	  Matter.Body.applyForce(paperBallobj.body,paperBallobj.body.position,{x:130, y:-145});  
+	}
+}
